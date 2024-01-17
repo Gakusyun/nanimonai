9a401d783989f5e39cef35a2170655557cf4bac9
Gakusyun <Gaoxj040620@outlook.com>
2024-01-17 18:26:06 +0800

    kansei

--- cpro/datastruct/E-Accounts/cmake-build-debug/cmake_install.cmake
+++ cpro/datastruct/E-Accounts/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/E-Accounts
+# Install script for directory: D:/nanimonai/cpro/datastruct/E-Accounts
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/E-Accounts/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/nanimonai/cpro/datastruct/E-Accounts/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
