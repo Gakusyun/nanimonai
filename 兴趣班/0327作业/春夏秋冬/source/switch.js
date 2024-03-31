7996ebb625d4ab0df81d5f7ef318c48e3261264a
Gakusyun <67746154+Gakusyun@users.noreply.github.com>
2024-03-31 12:17:45 +0800

    Add files via upload

---
+++ 兴趣班/0327作业/春夏秋冬/source/switch.js
@@ -0,0 +1,16 @@
+function chun() {
+    var image = document.getElementById("img");
+    image.src = "./images/chun.jpg";
+}
+function xia() {
+    var image = document.getElementById("img");
+    image.src = "./images/xia.jpg";
+}
+function qiu() {
+    var image = document.getElementById('img');
+    image.src = './images/qiu.jpg';
+}
+function dong() {
+    var image = document.getElementById('img');
+    image.src = './images/dong.jpg';
+}
