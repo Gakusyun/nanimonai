3f43102c4dc33a78f78c12b8ade112e952ded84e
Gakusyun <gaoxj040620@outlook.com>
2024-05-24 14:41:14 +0800

    add while and don't stop

--- discreteMathematics/Shiyan_2/main.c
+++ discreteMathematics/Shiyan_2/main.c
@@ -2,15 +2,15 @@
 #include "SETLOGICHEADER.h"
 #include "SETLOGICHEADER.c"
 
-
 int main()
 {
-	//集合的链表表示测试
-	//sample_linkset_test();
+	// 集合的链表表示测试
+	// sample_linkset_test();
 
-	//集合的数组表示测试
-
-    sample_arrayset_test();
-	return 0;
-
+	// 集合的数组表示测试
+	while (1)
+	{
+		sample_arrayset_test();
+		return 0;
+	}
 }
